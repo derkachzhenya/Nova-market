dc1eca88a933b99a502f7397fc5e0d4164f6cd96
zhenya457 <zheka4573@gmail.com>
2026-05-13 16:51:37 +0300

    add dashboard layout and controller for admin panel

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <section class="w-full p-4 border-b border-gray-100">
+        <header>
+            Market Admin
+        </header>
+    </section>
+
+    <section class="flex">
+
+        <aside class="w-1/4 min-h-screen">
+            <nav> </nav>
+        </aside>
+
+        <article class="w-3/4 bg-gray-50">
+            <slot />
+        </article>
+
+    </section>
+
+    <section>
+        <footer>
+
+        </footer>
+    </section>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped></style>
---
+++ resources/js/Pages/Admin/Dashboard/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Dashboard Index 
+    </div>
+</template>
+
+
+<script>
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+
+export default {
+    layout: AdminLayout,
+}
+</script>
+
+<style scoped></style> 
